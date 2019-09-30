e44c2d583dab6d0f043856cbbe144fef4ae2df3c
Enrique Cervino <enrique.cervirodam@gmail.com>
2019-09-30 16:48:35 +0200

    5th commit nothing remarcable

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { DataService } from './dataservice';
-import { SorterService } from './sortservice';
+import { SorterService } from './sorterservice';
 import { from } from 'rxjs';
 
 @NgModule({
--- src/app/core/dataservice.ts
+++ src/app/core/dataservice.ts
@@ -1,16 +1,58 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-import { Observable } from "rxjs";
+import { Observable, of, from } from "rxjs";
 import { map, catchError } from "rxjs/operators";
 
 import { ICustomer, IOrder } from "../../app/shared/interfaces";
+import { Icu } from "@angular/compiler/src/i18n/i18n_ast";
 
 @Injectable()
 export class DataService {
+  /**
+   * SI TRABAJAMOS CON FORMATO JSON DA IGUAL
+   * DE DONDE VENGA, CON TAL DE SETTEAR BIEN
+   * LA BASE URL SERÍA SUFICIENTE.
+   */
   baseUrl: string = "assets/";
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Mth. que devuelve los clientes, accede a 
+   * la URL y al estar en JSON observable es 
+   * buena manera de hacerlo.
+   */
+  getCustomers(): Observable<ICustomer[]> {
+    return this.http
+      .get<ICustomer[]>(this.baseUrl + "customers.json")
+      .pipe(catchError(this.handleError));
+  }
+
+  /**
+   * 
+   */
+  getCustomer(id: number): Observable<ICustomer> {
+    return this.http.get<ICustomer[]>(this.baseUrl + "customers.json").pipe(
+      map(customers => {
+        let customer = customers.filter((cust: ICustomer) => cust.id === id);
+        return customer && customer.length ? customer[0] : null;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  getOrders(id: number): Observable<IOrder[]> {
+    return this.http.get<IOrder[]>(this.baseUrl + "orders.json").pipe(
+      map(orders => {
+        let custOrders = orders.filter(
+          (order: IOrder) => order.customerId === id
+        );
+        return custOrders;
+      }),
+      catchError(this.handleError)
+    );
+  }
 
   private handleError(error: any) {
     console.error("server error:", error);
